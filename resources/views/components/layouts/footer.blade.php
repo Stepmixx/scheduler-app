27a74eb771fa0aaf5357a9413c1cc98bed193126
Stephan <stepmix123@gmail.com>
2022-07-14 06:24:27 -0400

    added navbar and footer components

---
+++ resources/views/components/layouts/footer.blade.php
@@ -0,0 +1,6 @@
+<footer>
+    <p>Copyrigth © 2021 Stephan Calderin</p>
+    <p>Universidad de Margarita</p>
+    <p>Ingenieria de Sistemas</p>
+    <p>Programación IV</p>
+</footer>
---
+++ resources/views/components/layouts/nav-bar.blade.php
@@ -0,0 +1,17 @@
+<nav class="navbar">
+    <div class="logo">
+        <a href="/home">
+            <img src="{{ asset('img/scheduler-logo.svg') }}" alt="scheduler-logo" class="logo-img" />
+            <p class="logo-text">Scheduler</p>
+        </a>
+    </div>
+    <ul class="navLinks">
+        <li><a href="{{ route('shifts') }}">Turnos</a></li>
+        <li><a href="{{ route('disponibilities') }}">Disponibilidad</a></li>
+        <li>
+            <a href="{{ route('logout') }}">
+                <img src="{{ asset('img/logout.svg') }}" alt="logout-ico" class="logout" />
+            </a>
+        </li>
+    </ul>
+</nav>
