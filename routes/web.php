3e2bcab75fbe6e4963a4afb5a8daa8297269e41e
Stephan <stepmix123@gmail.com>
2022-07-14 06:29:32 -0400

    added routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,14 +13,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [AuthController::class, 'redirect'])->name('redirect');
 
-Route::get('/signup', function () {
-    return view('auth.signup');
-});
+Route::get('/signup',[AuthController::class, 'signup'])->name('signup')->middleware('alreadyLoggedIn');
 
-Route::get('/login', function () {
-    return view('auth.login');
-});
+Route::get('/login', [AuthController::class, 'login'])->name('login')->middleware('alreadyLoggedIn');
+
+Route::get('/home', [AuthController::class, 'homePage'])->name('home')->middleware('isLoggedIn');
+
+Route::get('/shifts', [AuthController::class, 'shifts'])->name('shifts')->middleware('isLoggedIn');
+
+Route::get('/disponibilities', [AuthController::class, 'disponibilities'])->name('disponibilities')->middleware('isLoggedIn');
+
+Route::get('/users', [AuthController::class, 'users'])->name('users')->middleware('isLoggedIn');
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::post('signup-user',[AuthController::class, 'signupUser'])->name('signup-user');
+
+Route::post('login-user',[AuthController::class, 'loginUser'])->name('login-user');
