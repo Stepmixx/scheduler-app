7d25c23f4d5da28a600d0f09ad441b1e61d8eb5d
Stephan <stepmix123@gmail.com>
2022-07-14 06:20:24 -0400

    added user and roles migrations

--- database/migrations/2022_07_14_052323_create_users_table.php
+++ database/migrations/2022_07_14_052323_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 return new class extends Migration
 {
@@ -16,12 +18,22 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('lastname');
+            $table->string('identification');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('role_id')->default(1);
             $table->timestamps();
         });
+
+        $user = new User();
+        $user->name = 'Georgelys';
+        $user->lastname = 'Marcano';
+        $user->identification = 'V28492648';
+        $user->email = 'master@email.com';
+        $user->password = Hash::make('123456');
+        $user->role_id = 2;
+        $user->save();
     }
 
     /**
--- database/migrations/2022_07_14_082013_create_roles_table.php
+++ database/migrations/2022_07_14_082013_create_roles_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Role;
 
 return new class extends Migration
 {
@@ -13,15 +14,16 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->timestamps();
         });
+
+        $roles = ['Usuario', 'Administrador'];
+        foreach($roles as $role) {
+            Role::create([ 'name' => $role ]);
+        }
     }
 
     /**
@@ -31,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('roles');
     }
 };
